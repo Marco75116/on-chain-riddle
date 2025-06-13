0fab220f9a44fb3fd3e239f4ca75e3113dcf4960
Marco75116 <ourradourm@gmail.com>
2025-06-13 17:52:00 +0200

    update realtime riddles

--- riddle-app/lib/hooks/useRiddles.hook.ts
+++ riddle-app/lib/hooks/useRiddles.hook.ts
@@ -1,9 +1,29 @@
 import { useQuery } from "@apollo/client";
-
 import { GET_RIDDLES } from "../graphql/queries.graphql";
+import { useEffect } from "react";
+import { WEBSOCKET_URL } from "../constants/global.constant";
 
 export function useRiddles() {
-  const { loading, error, data } = useQuery(GET_RIDDLES);
+  const { loading, error, data, refetch } = useQuery(GET_RIDDLES);
+
+  useEffect(() => {
+    const ws = new WebSocket(WEBSOCKET_URL);
+
+    ws.onmessage = (event) => {
+      const message = event.data;
+      if (message === "New riddle available!") {
+        refetch();
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [refetch]);
 
   return {
     riddles: data?.riddles || [],
